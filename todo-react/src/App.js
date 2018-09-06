027547409f672c9c14b9206240e2ee2ec5d0f9c1
Doondoony <cream.doondoon@gmail.com>
2018-09-07 02:01:26 +0900

    Styling Todo Item

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import styled from "styled-components";
 import TodoItem from "./components/TodoItem";
+import { Map, List, Record } from "immutable";
+
+
+const Container = styled.div`
+  padding: 10px;
+`;
 
 const InputContainer = styled.div`
-  padding: 10px;
 `;
 
 const InputForm = styled.form`
@@ -14,8 +19,10 @@ const InputTodo = styled.input`
   display: inline-block;
   width: 100%;
   height: 40px;
+  padding: 10px;
   border: 1px solid #BBB;
   border-radius: 3px;
+  font-size: 14px;
   &::selection {
     background: #54da64;
   }
@@ -26,7 +33,7 @@ const StyledAddButton = styled.button`
   height: 40px;
   line-height: 0;
   top: 50%;
-  right: -3px;
+  right: 0;
   transform: translateY(-50%);
   border-radius: 3px;
   border: none;
@@ -36,43 +43,56 @@ const StyledAddButton = styled.button`
 `;
 
 const TodoItems = styled.ul`
-  
+  width: 100%;
 `;
 
 
 class App extends Component {
+
+  static Todo = Record({ id: null, whatTodo: null, checked: false });
+
+  state = {
+    id: 0,
+    todoItems: List([ new App.Todo({ id: 999, whatTodo: "디디 귀여워하기", checked: false }) ])
+  };
+
   constructor(props) {
     super(props);
-    this.state = {
-      todoItems: []
-    };
   }
 
   handleSubmit(e) {
     const { todo } = e.target.elements;
-    this.setState({ todoItems: [ ...this.state.todoItems, { whatTodo: todo.value, completed: false } ] });
-    e.preventDefault();
+    const todoItem = new App.Todo({ id: this.state.id, whatTodo: todo.value, checked: false });
+    this.setState({ id: this.state.id + 1 });
+    this.setState({ todoItems: this.state.todoItems.push(todoItem) });
     todo.value = '';
+    e.preventDefault();
   }
 
-  handleComplete(fromChild) {
-    console.log(fromChild);
-  }
+  toggleCheckbox = (index) => {
+    const { todoItems } = this.state;
+    const currentStatus = todoItems.get(index).get('checked');
+    this.setState({ todoItems: todoItems.update(index, todo => todo.set('checked', !currentStatus)) });
+  };
 
   render() {
     return (
       <Fragment>
-        <InputContainer>
-          <InputForm onSubmit={e => this.handleSubmit(e)}>
-            <InputTodo type="text" name="todo" placeholder="What should I do..."/>
-            <StyledAddButton type="submit">
-              ADD
-            </StyledAddButton>
-          </InputForm>
-        </InputContainer>
-        <TodoItems>
-          {this.state.todoItems.map((todoItem, index) => <TodoItem key={index} onChange={this.handleComplete} {...todoItem} />)}
-        </TodoItems>
+        <Container>
+          <InputContainer>
+            <InputForm onSubmit={e => this.handleSubmit(e)}>
+              <InputTodo type="text" name="todo" placeholder="What should I do..."/>
+              <StyledAddButton type="submit">
+                ADD
+              </StyledAddButton>
+            </InputForm>
+          </InputContainer>
+          <TodoItems>
+            {this.state.todoItems.map((todo, index) => (
+              <TodoItem key={index} onChange={this.toggleCheckbox.bind(this, index)} {...todo.toJS()} />
+            ))}
+          </TodoItems>
+        </Container>
       </Fragment>
     );
   }
--- todo-react/src/components/TodoItem.js
+++ todo-react/src/components/TodoItem.js
@@ -2,18 +2,69 @@ import React, { Component, Fragment } from 'react';
 import styled from "styled-components";
 
 
-const StyledCheckBox = styled.input`
-    
+const Todo = styled.li`
+  display: flex;
+  width: 100%;
+  height: 40px;
+  margin-top: 10px;
+  padding-left: 15px;
+  border: 1px solid #DDD;
+  box-shadow: 1px 1px 2px #DDD;
+  font-size: 14px;
 `;
+
+
+const StyledLabel = styled.label`
+  display: inline-flex;
+  align-items: center;
+  color: #444;
+  cursor: pointer;
+`;
+
+
+const StyledCheckBox = styled.input`
+  display: none;
+  
+  & + label:before {
+    display: inline-block;
+    content: '';
+    border: 1px solid rgba(0, 0, 0, .15);
+    width: 16px;
+    height: 16px;
+    margin-right: 15px;
+  }
+  &:hover + label:before {
+    transition: opacity 0.5s ease-in-out;
+    content: '\\2715';
+    font-size: 12px;
+    color: white;
+    border: 1px solid rgba(219, 1, 81, .15);
+    background-color: rgba(219, 47, 108, .15);
+    text-align: center;
+  }
+  &:checked + label:before {
+    content: '\\2715';
+    font-size: 12px;
+    color: white;
+    border: 1px solid #DB0151;
+    background-color: #DB2F6C;
+    text-align: center;
+  }
+  &:checked + label > span {
+    text-decoration: line-through;
+  }
+`;
+
 
 class TodoItem extends Component {
   render() {
+    const { id } = this.props;
     return (
       <Fragment>
-        <li>
-          <StyledCheckBox onChange={this.props.onChange.call(null, this.props)} type="checkbox" />
-          {this.props.whatTodo}
-        </li>
+        <Todo>
+          <StyledCheckBox id={id} onChange={this.props.onChange} type="checkbox"/>
+          <StyledLabel htmlFor={id}><span>{this.props.whatTodo}</span></StyledLabel>
+        </Todo>
       </Fragment>
     );
   }
