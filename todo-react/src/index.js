ea6a57049c9e5533e2c350262db38971b1641cbc
Doondoony <cream.doondoon@gmail.com>
2018-09-16 23:15:38 +0900

    Update FontFace

--- todo-react/src/index.js
+++ todo-react/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/SF-FontFace.css';
 import './styles/reset.css';
 import './styles/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
