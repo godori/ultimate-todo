ab2dea0479f35cbb20ee4478f325140a4ea31617
Doondoony <cream.doondoon@gmail.com>
2018-09-21 01:07:02 +0900

    Testing async event

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -17,17 +17,76 @@ class App extends Component {
   static ID = 0;
   static todoItem = { id: null, whatTodo: null, status: -1, startDate: null, endDate: null };
   static dateFormat = 'YYYY-MM-DD HH:mm:ss';
+  static DATABASE = 'TODO_APP';
+  static STORENAME = 'TODO';
+  static DB_VERSION = 1;
+  static IDB;
 
-  static scanLocalStorage() {
-    const todoItems = Object.keys(localStorage).map(key => JSON.parse(localStorage.getItem(key)));
-    if (todoItems) {
-      todoItems.forEach(todoItem => {
-        todoItem.startDate = dateFns.format(dateFns.parse(todoItem.startDate), App.dateFormat);
-        todoItem.endDate = dateFns.format(dateFns.parse(todoItem.endDate), App.dateFormat);
-      });
-    }
-    return todoItems
-  }
+  addTodoItem = whatTodo => {
+    const ID = ++App.ID;
+    const now = dateFns.format(new Date(), App.dateFormat, koLocale);
+    const todoItem = { ...App.todoItem, id: ID, whatTodo, startDate: now, endDate: now };
+    localStorage.setItem(ID.toString(), JSON.stringify(todoItem));
+    this.setState({ todoItems: [ ...this.state.todoItems, todoItem ] });
+  };
+  removeTodoItem = id => {
+    localStorage.removeItem(id.toString());
+    this.setState({ todoItems: [ ...this.state.todoItems.filter(todoItem => todoItem.id !== id) ] });
+  };
+  changeStatusTodoItem = id => {
+    const updated = this.state.todoItems.map(todoItem => {
+      if (todoItem.id === id) {
+        todoItem.status *= -1;
+      }
+      return todoItem;
+    });
+    this.setState({ todoItems: [ ...updated ] });
+  };
+  filterTodoItem = tabName => {
+    const tabNameStatus = {
+      'ALL': 0,
+      'TODO': -1,
+      'DONE': 1
+    };
+    this.setState({ status: tabNameStatus[ tabName ] });
+  };
+  handleFormSubmit = e => {
+    const { whatTodo } = e.target.elements;
+    this.addTodoItem(whatTodo.value);
+    whatTodo.value = '';
+    e.preventDefault();
+  };
+  handleSearch = input => {
+    this.setState({ searchTerm: input });
+  };
+  hideOverLay = () => {
+    this.setState({ overLayVisible: false });
+  };
+  showOverLay = () => {
+    this.setState({ overLayVisible: true });
+  };
+  sortByDate = () => {
+    const { todoItems, sortType } = this.state;
+    const newSortType = sortType === 'desc' ? 'asc' : 'desc';
+    const sortingMethods = {
+      desc(a, b) {
+        return a.createdAt - b.createdAt;
+      },
+      asc(a, b) {
+        return b.createdAt - a.createdAt;
+      }
+    };
+    const sorted = [ ...todoItems ].sort(sortingMethods[ newSortType ]);
+    this.setState({ todoItems: [ ...sorted ], sortType: newSortType });
+  };
+
+  asyncEvent = async () => {
+    fetch('https://httpbin.org/get').then(res => {
+      const div = document.createElement('div');
+      div.innerText = JSON.stringify(res);
+      document.body.appendChild(div)
+    })
+  };
 
   constructor(props) {
     super(props);
@@ -42,71 +101,16 @@ class App extends Component {
     };
   }
 
-  addTodoItem = whatTodo => {
-    const ID = ++App.ID;
-    const now = dateFns.format(new Date(), App.dateFormat, koLocale);
-    const todoItem = { ...App.todoItem, id: ID, whatTodo, startDate: now, endDate: now };
-    localStorage.setItem(ID.toString(), JSON.stringify(todoItem));
-    this.setState({ todoItems: [ ...this.state.todoItems, todoItem ] });
-  };
-
-  removeTodoItem = id => {
-    localStorage.removeItem(id.toString());
-    this.setState({ todoItems: [ ...this.state.todoItems.filter(todoItem => todoItem.id !== id) ] });
-  };
-
-  changeStatusTodoItem = id => {
-    const updated = this.state.todoItems.map(todoItem => {
-      if (todoItem.id === id) {
-        todoItem.status *= -1;
-      }
-      return todoItem;
-    });
-    this.setState({ todoItems: [ ...updated ] });
-  };
-
-  filterTodoItem = tabName => {
-    const tabNameStatus = {
-      'ALL': 0,
-      'TODO': -1,
-      'DONE': 1
-    };
-    this.setState({ status: tabNameStatus[ tabName ] });
-  };
-
-  handleFormSubmit = e => {
-    const { whatTodo } = e.target.elements;
-    this.addTodoItem(whatTodo.value);
-    whatTodo.value = '';
-    e.preventDefault();
-  };
-
-  handleSearch = input => {
-    this.setState({ searchTerm: input });
-  };
-
-  hideOverLay = () => {
-    this.setState({ overLayVisible: false });
-  };
-
-  showOverLay = () => {
-    this.setState({ overLayVisible: true });
-  };
-
-  sortByDate = () => {
-    const { todoItems, sortType } = this.state;
-    const newSortType = sortType === 'desc' ? 'asc' : 'desc';
-    const sortingMethods = {
-      desc(a, b) {
-        return a.createdAt - b.createdAt
-      },
-      asc(a, b) {
-        return b.createdAt - a.createdAt
-      }
-    };
-    const sorted = [...todoItems].sort(sortingMethods[newSortType]);
-    this.setState({ todoItems: [...sorted], sortType: newSortType });
-  };
+  static scanLocalStorage() {
+    const todoItems = Object.keys(localStorage).map(key => JSON.parse(localStorage.getItem(key)));
+    if (todoItems) {
+      todoItems.forEach(todoItem => {
+        todoItem.startDate = dateFns.format(dateFns.parse(todoItem.startDate), App.dateFormat);
+        todoItem.endDate = dateFns.format(dateFns.parse(todoItem.endDate), App.dateFormat);
+      });
+    }
+    return todoItems;
+  }
 
   render() {
     const mapToComponent = todoItems => {
@@ -141,6 +145,7 @@ class App extends Component {
             visible={this.state.overLayVisible}
             onClose={this.hideOverLay}
             onSubmit={this.handleFormSubmit}/>
+          <button onClick={this.asyncEvent}/>
         </Container>
       </Fragment>
     );
