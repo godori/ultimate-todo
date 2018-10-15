b55debae1c33edd2617e6690c00072e02047efe3
Doondoony <cream.doondoon@gmail.com>
2018-10-16 08:56:25 +0900

    TODO: JSX like > HTML

--- todo-vanilla-redux/src/index.js
+++ todo-vanilla-redux/src/index.js
@@ -1,10 +1,28 @@
-// actions
-const INCREMENT = 'INCREMENT';
-const DECREMENT = 'DECREMENT';
+((window, document) => {
+  const createElementFromHtmlString = htmlString => document.createRange().createContextualFragment(htmlString);
+  const render = () => {
+    const $display = document.createRange().createContextualFragment(`
+      <div>
+        <span>0</span>
+      </div>
+    `);
 
-// actionsTypes
-const increment = () => {
-  return {
-    type: INCREMENT,
+    const $plusButton = createElementFromHtmlString('<button class="plus">+</button>');
+    const $minusButton = createElementFromHtmlString('<button class="minus">-</button>');
+    const $buttons = ((...$buttons) => {
+      const $container = document.createDocumentFragment();
+      const $div = createElementFromHtmlString('<div></div>');
+      $container.appendChild($div);
+      $container.querySelector();
+    })($plusButton, $minusButton);
+
+    const wrap = (...$elements) => {
+      const $container = document.querySelector('#app');
+      $elements.forEach($element => $container.appendChild($element));
+    };
+
+    wrap($display, $buttons);
   };
-};
+
+  render();
+})(window, document);
---
+++ todo-vanilla-redux/src/store.js
@@ -0,0 +1,38 @@
+// actions
+import { createStore } from 'redux';
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+// actionsTypes
+const increment = () => ({
+  type: INCREMENT,
+});
+
+const decrement = () => ({
+  type: DECREMENT,
+});
+
+const initialState = {
+  counter: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+
+export { store, increment, decrement };
